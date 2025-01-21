1e40c8efdd37504c726fd29ef46f33718cec00e2
Charles Ouimet <charles.ouimet@gmail.com>
2025-01-21 16:19:25 -0500

    [02:25:59] Add behaviour so the `Buy` button adjusts to who is connected to the site: owner, inspector, lender, seller or buyer

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 
 import close from '../assets/close.svg';
 
-const Home = ({ home, provider, escrow, togglePop }) => {
+const Home = ({ home, provider, account, escrow, togglePop }) => {
     const [hasBought, setHasBought] = useState(false)
     const [hasLended, setHasLended] = useState(false)
     const [hasInspected, setHasInspected] = useState(false)
@@ -78,14 +78,37 @@ const Home = ({ home, provider, escrow, togglePop }) => {
               <p>{home.address}</p>
               <h2>{home.attributes[0].value} ETH</h2>
 
-              <div>
-                <button className="home__buy">
-                  Buy
-                </button>
-              </div>
-              <button className="home__contact">
-                Contact agent
-              </button>
+              {owner ? (
+                <div className="home__owned">
+                  Owned by {owner.slice(0, 6) + '...' + owner.slice(38, 42)}
+                </div>
+              ) : (
+                <div>
+                  {(account === inspector) ? (
+                    <button className="home__buy">
+                      Approve Inspection
+                    </button>
+                  
+                  ) : (account === lender) ? (
+                    <button className="home__buy">
+                      Approve & Lend
+                    </button>
+                    
+                  ) : (account === seller) ? (
+                    <button className="home__buy">
+                      Approve & Sell
+                    </button>
+                  ): (
+                    <button className="home__buy">
+                      Buy
+                    </button>
+                  )}
+
+                  <button className="home__contact">
+                    Contact agent
+                  </button>
+                </div>
+              )}
 
               <hr />
 
