c890b10af29e4bd371a852d1cb860509238455da
Charles Ouimet <charles.ouimet@gmail.com>
2025-01-21 15:02:33 -0500

    [01:17:44] Add transfer of NFT ownership in `Escrow.finalizeSale()` and tests

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -100,5 +100,7 @@ contract Escrow {
 
         (bool success,) = payable(seller).call{value: address(this).balance}("");
         require(success);
+
+        IERC721(nftAddress).transferFrom(address(this), buyer[_nftID], _nftID);
     }
 }
