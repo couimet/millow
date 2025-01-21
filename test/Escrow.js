03798b2b732b1b325160bdf328ab5abcce9e8cfa
Charles Ouimet <charles.ouimet@gmail.com>
2025-01-21 14:06:41 -0500

    [00:47:48] Add `Escrow.list()` method with test

--- test/Escrow.js
+++ test/Escrow.js
@@ -29,6 +29,14 @@ describe('Escrow', () => {
       inspector.address,
       lender.address
     )
+    
+    // Approve Property
+    transaction = await realEstate.connect(seller).approve(escrow.address, 1)
+    await transaction.wait()
+  
+    // List property
+    transaction = await escrow.connect(seller).list(1);
+    await transaction.wait()
   })
 
 describe('Deployment', () => {
@@ -50,6 +58,12 @@ describe('Deployment', () => {
     it('Returns lender', async () => {
       const result = await escrow.lender()
       expect(result).to.be.equal(lender.address)
+    })
+  })
+
+  describe('Listing', () => {
+    it('Updates ownership', async () => {
+      expect(await realEstate.ownerOf(1)).to.be.equal(escrow.address)
     })
   })
 })
