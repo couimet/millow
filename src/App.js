16b19d28185e47982a61cc5f88fcb48337eabc67
Charles Ouimet <charles.ouimet@gmail.com>
2025-01-21 15:25:09 -0500

    [01:47:27] The `Navigation` component adjusts the connected address when we switch accounts in Metamask

--- src/App.js
+++ src/App.js
@@ -19,6 +19,12 @@ function App() {
 
   const loadBlockchainData = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum)
+
+    window.ethereum.on('accountsChanged', async () => {
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const account = ethers.utils.getAddress(accounts[0])
+      setAccount(account)
+    })
   }
 
   useEffect(() => {
