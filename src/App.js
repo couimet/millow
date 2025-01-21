4731e12c12c7ed51004946b64d6e64f15033ec45
Charles Ouimet <charles.ouimet@gmail.com>
2025-01-21 15:33:25 -0500

    [01:59:28] Load the homes from the blockchain and `console.log()` them

--- src/App.js
+++ src/App.js
@@ -15,10 +15,33 @@ import config from './config.json';
 
 function App() {
 
+  const [provider, setProvider] = useState(null)
+  const [escrow, setEscrow] = useState(null)
+
   const [account, setAccount] = useState(null)
+  const [homes, setHomes] = useState([])
 
   const loadBlockchainData = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider)
+
+    const network = await provider.getNetwork()
+
+    const realEstate = new ethers.Contract(config[network.chainId].realEstate.address, RealEstate, provider)
+    const totalSupply = await realEstate.totalSupply()
+    const homes = []
+
+    for (var i = 1; i <= totalSupply; i++) {
+      const uri = await realEstate.tokenURI(i)
+      const response = await fetch(uri)
+      const metadata = await response.json()
+      homes.push(metadata)
+    }
+    setHomes(homes)
+    console.log(homes)
+
+    const escrow = new ethers.Contract(config[network.chainId].escrow.address, Escrow, provider)
+    setEscrow(escrow)
 
     window.ethereum.on('accountsChanged', async () => {
       const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
