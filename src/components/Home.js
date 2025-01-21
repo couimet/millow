66b657ab414363385b5c5b4ff363847636ebcf7d
Charles Ouimet <charles.ouimet@gmail.com>
2025-01-21 16:41:53 -0500

    [02:32:14] Implement behaviour in `buyHandler()`, `inspectHandler()`, `lendHendler()` and `sellHandler()`

--- src/components/Home.js
+++ src/components/Home.js
@@ -58,15 +58,56 @@ const Home = ({ home, provider, account, escrow, togglePop }) => {
     }
 
     const buyHandler = async () => {
+      const escrowAmount = await escrow.escrowAmount(home.id)
+      const signer = await provider.getSigner()
+
+      // Buyer deposit earnest
+      let transaction = await escrow.connect(signer).depositEarnest(home.id, { value: escrowAmount })
+      await transaction.wait()
+
+      // Buyer approves...
+      transaction = await escrow.connect(signer).approveSale(home.id)
+      await transaction.wait()
+
+      setHasBought(true)
     }
 
     const inspectHandler = async () => {
+      const signer = await provider.getSigner()
+
+      // Inspector updates status
+      const transaction = await escrow.connect(signer).updateInspectionStatus(home.id, true)
+      await transaction.wait()
+
+      setHasInspected(true)      
     }
 
     const lendHandler = async () => {
+      const signer = await provider.getSigner()
+
+      // Lender approves...
+      const transaction = await escrow.connect(signer).approveSale(home.id)
+      await transaction.wait()
+
+      // Lender sends funds to contract...
+      const lendAmount = (await escrow.purchasePrice(home.id) - await escrow.escrowAmount(home.id))
+      await signer.sendTransaction({ to: escrow.address, value: lendAmount.toString(), gasLimit: 60000 })
+
+      setHasLended(true)
     }
 
     const sellHandler = async () => {
+      const signer = await provider.getSigner()
+
+      // Seller approves...
+      let transaction = await escrow.connect(signer).approveSale(home.id)
+      await transaction.wait()
+
+      // Seller finalize...
+      transaction = await escrow.connect(signer).finalizeSale(home.id)
+      await transaction.wait()
+  
+      setHasSold(true)
     }
 
     useEffect(() => {
