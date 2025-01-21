c15219011ca5e71e257dce5f42510fd37b70387e
Charles Ouimet <charles.ouimet@gmail.com>
2025-01-21 13:53:54 -0500

    [00:33:40] Start building tests for `Escrow` smart contract

--- test/Escrow.js
+++ test/Escrow.js
@@ -6,5 +6,29 @@ const tokens = (n) => {
 }
 
 describe('Escrow', () => {
+  let buyer, seller, inspector, lender;
+  let realEstate, escrow;
+  
+  it('saves the adresses ', async () => {
 
+    // Setup accounts
+    [ buyer, seller, inspector, lender ] = await ethers.getSigners();
+
+    // Deploy Real Estate
+    const RealEstate = await ethers.getContractFactory('RealEstate');
+    realEstate = await RealEstate.deploy();
+    
+    // Mint
+    let transaction = await realEstate.connect(seller).mint('https://ipfs.io/ipfs/QmTudSYeM7mz3PkYEWXWqPjomRPHogcMFSq7XAvsvsgAPS');
+    await transaction.wait();
+    
+    const Escrow = await ethers.getContractFactory('Escrow');
+    escrow = await Escrow.deploy(realEstate.address, seller.address, inspector.address, lender.address); 
+
+    let result = await escrow.nftAddress();
+    expect(result).to.be.equal(realEstate.address);
+
+    result = await escrow.seller();
+    expect(result).to.be.equal(seller.address);
+  })
 })
